cce574d66c1a4d786f6afcedf9f71e09cf8e1436
hani <haniyousfi@gmail.com>
2020-11-26 23:22:32 +0100

    Edit main.py

--- main.py
+++ main.py
@@ -7,31 +7,26 @@ from train_model import TrainModel
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', '--mode', required=True,
                     help='Run training mode or testing mode')
-parser.add_argument('-a', '--audio', default='Train',
-                    help='The path to the audio files')
-parser.add_argument('-d', '--destination', default='spectrogram',
-                    help='Where to save the spectrograms of the audio files')
-parser.add_argument('-c', '--csv', default='train.csv',
-                    help='The path to the CSV file')
-parser.add_argument('-t', '--train_data', defaul='train_data.npy',
-                    help='The path the npy file')
 args = parser.parse_args()
 mode = args.mode
-audio_path = args.audio
-destination_folder = args.destination
-csv_file = args.csv
-train_data = args.train_data
 
 if mode == 'train':
+    audio_path = input('Please enter the audio files path: ')
+    csv_file = input('Please enter the csv_file path: ')
+    destination_folder = input(
+        'Please enter the folder where the spectrograms will be saved: ')
+    train_data = input(
+        'Please enter the name of the npy file (train_data.npy): ')
     prepare_data(audio_path=audio_path,
                  destination_folder=destination_folder,
-                 csv_file=csv_file)
+                 csv_file=csv_file,
+                 npy_file=train_data)
     train = TrainModel(data_path=train_data)
     train.run_training()
 elif mode == 'test':
-    test_audio = input('Please enter the audio file path')
-    weights_path = input('Please enter the weights path')
-    json_file = input('Please enter the json file path')
+    test_audio = input('Please enter the audio file path: ')
+    weights_path = input('Please enter the weights path: ')
+    json_file = input('Please enter the json file path: ')
     test = TestModel(audio_file=test_audio,
                      weights_path=weights_path,
                      json_path=json_file)
