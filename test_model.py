84204aa8a896199a06ae505e92192ec79987734e
hani <haniyousfi@gmail.com>
2020-11-26 23:35:19 +0100

    Add docstrings

--- test_model.py
+++ test_model.py
@@ -6,6 +6,27 @@ import tensorflow as tf
 
 
 class TestModel:
+    """Class to load the saved model and test it.
+
+    Parameters
+    ----------
+    audio_file : str
+        The path to the audio file to be tested.
+    weights_path : str
+        The path to the saved weights.
+    json_path : str
+        The path to the json file containing the model architecture.
+
+    Attributes
+    ----------
+    classes : list
+        The labels.
+    audio_file
+    weights_path
+    json_path
+
+    """
+
     def __init__(self, audio_file, weights_path, json_path):
         self.audio_file = audio_file
         self.weights_path = weights_path
@@ -15,6 +36,14 @@ class TestModel:
                         'air_conditioner', 'jackhammer', 'car_horn']
 
     def load_model(self):
+        """Load the model.
+
+        Returns
+        -------
+        model: tf.keras.models
+            The loaded model.
+
+        """
         json_file = open(self.json_path, 'r')
         loaded_json_model = json_file.read()
         model = tf.keras.models.model_from_json(loaded_json_model)
@@ -22,6 +51,14 @@ class TestModel:
         return model
 
     def process_data(self):
+        """Extract the spectrogram from the input audio file.
+
+        Returns
+        -------
+        spectrogram: numpy ndarray
+            The spectrogram of the input audio file.
+
+        """
         audio_ts, sample_rate = librosa.load(self.audio_file, sr=None,
                                              res_type='kaiser_fast')
         S = librosa.feature.melspectrogram(y=audio_ts, sr=sample_rate)
@@ -31,6 +68,13 @@ class TestModel:
         return spectrogram
 
     def run_test(self):
+        """predict the category of the input audio file.
+
+        Returns
+        -------
+        None.
+
+        """
         config = tf.compat.v1.ConfigProto()
         config.gpu_options.per_process_gpu_memory_fraction = 0.7
         tf.compat.v1.keras.backend.set_session(
